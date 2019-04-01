c96e99d4fd9b7128e99dcf9434cf1532cdea78a3
GerminalCorreia <GerminalCorreia@users.noreply.github.com>
2019-04-01 19:27:52 +0200

    connection to exchangeratesapi

--- webhook.js
+++ webhook.js
@@ -2,17 +2,39 @@
 const Restify = require('restify');
 const server = Restify.createServer({
   name: "MoneyBot"
-})
+});
+const request = require('request');
 const PORT = process.env.PORT || 3000;
 
 server.use(Restify.plugins.bodyParser());
 server.use(Restify.plugins.jsonp());
 
+const convertCurrency = (amountToConvert, outputCurrency, cb) => {
+  const {
+    amount,
+    currency
+  } = amountToConvert;
+  return request({
+    url: "https://api.exchangeratesapi.io/latest",
+    qs: {
+      base: currency,
+      symbols: outputCurrency
+    },
+    method: 'GET',
+    json: true
+  }, (error, response, body) => {
+    if(!error && response.statusCode === 200) {
+      let computedValue = Math.round(body.rates[outputCurrency] * amount);
+      cb(null, amount.toString() + currency + ' converts to about ' + outputCurrency + computedValue + ' as per current rates !');
+    } else {
+      cb(error, null);
+    }
+  });
+}
+
 // POST route handler
 server.post('/', (req, res, next) => {
   let result = req.body.queryResult;
-
-  console.log(result);
 
   if(result.action === 'convert') {
     const {amountToConvert, outputCurrency} = result.parameters;
@@ -26,6 +48,16 @@ server.post('/', (req, res, next) => {
         "fulfillmentText": responseText,
       }
       res.send(response);
+    } else {
+      // Query the fixer.io API to fetch and create a response
+      convertCurrency(amountToConvert, outputCurrency, (error, result) => {
+        if(!error && result) {
+          let response = {
+            "fulfillmentText": result,
+          }
+          res.send(response);
+        }
+      })
     }
   }
 
