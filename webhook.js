5c895eeb863d8bbb7cfea20f3e3980d5637bbe39
GerminalCorreia <GerminalCorreia@users.noreply.github.com>
2019-04-01 17:28:52 +0200

    print parameters

--- webhook.js
+++ webhook.js
@@ -10,12 +10,13 @@ server.use(Restify.plugins.jsonp());
 
 // POST route handler
 server.post('/', (req, res, next) => {
-  let {
-    status,
-    result
-  } = req.body;
+  let result = req.body.queryResult;
+  const {amountToConvert, outputCurrency} = result.parameters;
 
-  console.log(result);
+  console.log(amountToConvert);
+  console.log(outputCurrency);
 
   return next();
 });
+
+server.listen(PORT, () => console.log('Moneybot running on PORT: ' + PORT));
