09783374780c4050f7fe276715ed8e7968ca32f8
GerminalCorreia <GerminalCorreia@users.noreply.github.com>
2019-04-01 18:43:34 +0200

    response do dialogflow

--- webhook.js
+++ webhook.js
@@ -11,10 +11,23 @@ server.use(Restify.plugins.jsonp());
 // POST route handler
 server.post('/', (req, res, next) => {
   let result = req.body.queryResult;
-  const {amountToConvert, outputCurrency} = result.parameters;
 
-  console.log(amountToConvert);
-  console.log(outputCurrency);
+  console.log(result);
+
+  if(result.action === 'convert') {
+    const {amountToConvert, outputCurrency} = result.parameters;
+
+    // Check if input currency code === output currency code
+    if(amountToConvert.currency === outputCurrency) {
+      const {amount, currency} = amountToConvert;
+
+      let responseText = 'Well, ' + amount.toString() + currency + ' is obviously equal to ' + amount.toString() + currency;
+      let response = {
+        "fulfillmentText": responseText,
+      }
+      res.send(response);
+    }
+  }
 
   return next();
 });
